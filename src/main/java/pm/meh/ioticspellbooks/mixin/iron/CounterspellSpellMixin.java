0b08296a6ed544b49e227cbb807a64ed1f352ec1
Mephody <mephodio@gmail.com>
2025-11-15 01:23:28 +0300

    Attempt to clear casting data on counterspell

--- src/main/java/pm/meh/ioticspellbooks/mixin/iron/CounterspellSpellMixin.java
+++ src/main/java/pm/meh/ioticspellbooks/mixin/iron/CounterspellSpellMixin.java
@@ -7,7 +7,9 @@ import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
 import at.petrak.hexcasting.api.casting.iota.GarbageIota;
 import at.petrak.hexcasting.api.casting.iota.IotaType;
 import at.petrak.hexcasting.api.casting.mishaps.Mishap;
+import at.petrak.hexcasting.common.msgs.MsgClearSpiralPatternsS2C;
 import at.petrak.hexcasting.common.msgs.MsgNewSpellPatternS2C;
+import at.petrak.hexcasting.common.msgs.MsgOpenSpellGuiS2C;
 import at.petrak.hexcasting.xplat.IXplatAbstractions;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.spells.CastSource;
@@ -36,25 +38,23 @@ public class CounterspellSpellMixin {
             locals = LocalCapture.CAPTURE_FAILSOFT)
     void onCastOnPlayer(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData, CallbackInfo ci,
                         Vec3 localStart, Vec3 localEnd, HitResult localHitResult) {
-        if (localHitResult instanceof EntityHitResult entityHitResult &&
-                entityHitResult.getEntity() instanceof ServerPlayer serverPlayer) {
-            serverPlayer.sendSystemMessage(Component.literal("got you"));
+        if (!world.isClientSide
+                && localHitResult instanceof EntityHitResult entityHitResult
+                && entityHitResult.getEntity() instanceof ServerPlayer serverPlayer) {
             var vm = IXplatAbstractions.INSTANCE.getStaffcastVM(serverPlayer, InteractionHand.MAIN_HAND);
-            var image = vm.getImage();
-            var stack = image.getStack();
-            stack.add(new GarbageIota());
+            if (!vm.getImage().getStack().isEmpty()) {
+                IXplatAbstractions.INSTANCE.clearCastingData(serverPlayer);
+                var packet = new MsgClearSpiralPatternsS2C(serverPlayer.getUUID());
+                IXplatAbstractions.INSTANCE.sendPacketToPlayer(serverPlayer, packet);
+                IXplatAbstractions.INSTANCE.sendPacketTracking(serverPlayer, packet);
 
-//            vm.performSideEffects(List.of(new OperatorSideEffect.DoMishap(new MishapCounterspell(), new Mishap.Context(null, null))));
+                var patterns = IXplatAbstractions.INSTANCE.getPatternsSavedInUi(serverPlayer);
+                var ravenmind = vm.getImage().getUserData().contains(HexAPI.RAVENMIND_USERDATA) ?
+                        vm.getImage().getUserData().getCompound(HexAPI.RAVENMIND_USERDATA) : null;
 
-            ExecutionClientView clientInfo = new ExecutionClientView(
-                false,
-                ResolvedPatternType.ERRORED,
-                stack.stream().map(IotaType::serialize).toList(),
-                image.getUserData().contains(HexAPI.RAVENMIND_USERDATA) ?
-                        image.getUserData().getCompound(HexAPI.RAVENMIND_USERDATA) : null
-            );
-            IXplatAbstractions.INSTANCE.sendPacketToPlayer(serverPlayer,
-                    new MsgNewSpellPatternS2C(clientInfo, -1));
+                IXplatAbstractions.INSTANCE.sendPacketToPlayer(serverPlayer,
+                        new MsgOpenSpellGuiS2C(InteractionHand.MAIN_HAND, patterns, List.of(), ravenmind, 0));
+            }
         }
     }
 }
